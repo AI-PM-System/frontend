e592e225f9e86452d035410fdfc01ebe9f7383d4
Nicolai Andersen <niiicolai@gmail.com>
2023-04-22 01:40:04 +0200

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/AI-PM-System/frontend',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
